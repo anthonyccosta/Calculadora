53335b55f8f97718eecee5b146999d0e8b1058b6
Anthony Costa <ccosta.anthony@outlook.com>
2019-09-17 17:27:48 -0300

    Aprimoramento de layout e algumas inserções feitas no botão de numero.

--- Calculadora/Calculadora/MainPage.xaml.cs
+++ Calculadora/Calculadora/MainPage.xaml.cs
@@ -11,62 +11,66 @@ namespace Calculadora
     {
         public MainPage()
         {
-            InitializeComponent();
-        }
-
+            InitializeComponent();           
+        }                  
+        
         private void ButtonCalcular_Clicked(object sender, EventArgs e)
         {
+            calculadora cal = new calculadora();
+            cal.Somar();
 
+            
         }
 
         private void Btn7_Clicked(object sender, EventArgs e)
         {
-
+            entryNumero.Text += "7";
         }
 
         private void Btn8_Clicked(object sender, EventArgs e)
         {
-
+            entryNumero.Text += "8";
         }
 
         private void Btn9_Clicked(object sender, EventArgs e)
         {
-
+            entryNumero.Text += "9";
         }
 
         private void Btn6_Clicked(object sender, EventArgs e)
         {
-
+            entryNumero.Text += "6";
         }
 
         private void Btn5_Clicked(object sender, EventArgs e)
         {
-
+            entryNumero.Text += "5";
         }
 
         private void Btn4_Clicked(object sender, EventArgs e)
         {
-
+            entryNumero.Text += "4";
         }
 
         private void BtnMultiplicar_Clicked(object sender, EventArgs e)
         {
-
+            
         }
 
         private void Btn3_Clicked(object sender, EventArgs e)
         {
-
+            entryNumero.Text += "3";
+            
         }
 
         private void Btn2_Clicked(object sender, EventArgs e)
         {
-
+            entryNumero.Text += "2";
         }
 
         private void Btn1_Clicked(object sender, EventArgs e)
         {
-
+            entryNumero.Text += "1";
         }
 
         private void BtnSubtrair_Clicked(object sender, EventArgs e)
@@ -76,12 +80,12 @@ namespace Calculadora
 
         private void Btn0_Clicked(object sender, EventArgs e)
         {
-
+            entryNumero.Text += "0";
         }
 
         private void BtnLimpar_Clicked(object sender, EventArgs e)
         {
-
+            entryNumero.Text = "0";
         }
 
         private void BtnIgual_Clicked(object sender, EventArgs e)
---
+++ Calculadora/Calculadora/calculadora.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora
+{
+    public class calculadora
+    {
+        double valor1;
+        double valor2;
+        double resultado;
+        string op;
+        string label;
+
+        public void Somar()
+        {
+            
+            valor1 = Convert.ToDouble(label);
+            label = "";
+            op = "+";
+            
+        }
+    }
+}
